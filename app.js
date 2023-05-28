aff0100b4d680cc8efba2a69cdc01139f61de449
Karolis Šlipaitis <slipaitis.karolis@gmail.com>
2023-05-28 15:33:07 +0300

    add structure

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const logger = require('./middleware/logger');
 
+const rabbitRoutes = require('./rabbits/routes/rabbits.routes')
+
 const app = express()
 const port = 7070
 
 // logger middleware
 app.use(logger);
+
+// routes
+app.use('/rabbits', rabbitRoutes)
 
 // bellow optional:
 app.get('/', (req, res) => {
